4148702e3db0f42761e9e37e3948c9c0e3f01ae6
Szymik <szymonszymiczek32@gmail.com>
2021-06-27 19:22:54 +0200

    push

--- backend/models/Fpfti.php
+++ backend/models/Fpfti.php
@@ -45,4 +45,40 @@
             $stmt->execute();
             return $stmt;
         }
+
+        public function read_user_likes($user_id){
+            // $query = 'SELECT fpfti_id FROM likes l WHERE user_id = ' . $user_id . '';
+            // $stmt = $this->conn->prepare($query);
+
+            // $fpfti_arr = array();
+            // $fpfti_arr['data'] = array();
+            // while($fpfti_id = $stmt->execute()){
+            //     $query = 'SELECT * FROM ' . $this->table . ' f WHERE fpfti_id = ' . $fpfti_id . '';
+            //     $stmt2 = $this->conn->prepare($query);
+            //     array_push($fpfti_arr['data'], $stmt2->execute());
+            // }
+
+
+            // $query = 'SELECT * FROM ' . $this->table . ' f WHERE user_id = ' . $user_id . '';
+            // $stmt = $this->conn->prepare($query);
+            // $stmt->execute();
+            // return $stmt;
+
+            $query = 'SELECT f.id, f.title, f.user_id, f.link, f.accepted, f.likes, f.created
+                FROM fpfti as f
+                JOIN likes as l
+                ON f.id = l.fpfti_id
+                WHERE l.user_id = :user_id';
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function read_fpfti_comments($fpfti_id){
+            $query = 'SELECT * FROM comments c WHERE fpfti = '. $fpfti_id . 'ORDER BY f.created DESC';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
     }
