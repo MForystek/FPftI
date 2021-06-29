e041a1f86310312d3d89ed5478a424a6c3510a0e
Szymik <szymonszymiczek32@gmail.com>
2021-06-29 02:41:13 +0200

    search api

---
+++ backend/api/fpfti/read_search.php
@@ -0,0 +1,72 @@
+<?php
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Fpfti.php';
+
+    //Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate fpfti object
+    $fpfti = new Fpfti($db);
+
+    try {
+    $page = isset($_GET['page']) ? $_GET['page'] : die();
+    //$fpfti_tag = htmlspecialchars($_POST['tags']);
+    $fpfti_tag = '#smieszne';
+    
+
+    if (mb_strlen($fpfti_tag) !== 0) {
+            if (mb_strlen($fpfti_tag) > 64) {
+                echo json_encode(
+                    array('message' => 'hashtag too long')
+                );
+            }
+            if ($fpfti_tag[0] !== '#' || mb_strlen($fpfti_tag) <= 1) {
+                echo json_encode(
+                    array('message' => 'hash without hashtag')
+                );
+            }
+            $result = $fpfti->read_search($page, $fpfti_tag);
+
+            //get row count
+            $num = $result->rowCount();
+
+            //check if any fpfti
+            if($num > 0) {
+                //Post array
+                $fpfti_arr = array();
+                $fpfti_arr['data'] = array();
+
+                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    extract($row);
+                    $fpfti_item = array(
+                        'id' => $id,
+                        'title' => $title,
+                        'user_id' => $user_id,
+                        'link' => $link,
+                        'accepted' => $accepted,
+                        'likes' => $likes,
+                        'created' => $created
+                    );
+
+                    //Push to "data"
+                    array_push($fpfti_arr['data'], $fpfti_item);
+                }
+            }
+
+        //Turn to JSON & output
+        echo json_encode($fpfti_arr);
+    } else {
+        echo json_encode(
+            array('message' => 'No tags found')
+        );
+    }
+}catch (PDOException $e) {
+    echo json_encode(
+        array('error' => $e)
+    );
+}
--- backend/models/Fpfti.php
+++ backend/models/Fpfti.php
@@ -9,7 +9,7 @@
             $this->conn = $db;
         }
 
-        public function read_waiting($number){
+        public function read_waiting($number) {
             $first = ($number - 1)*5;
             $query = 'SELECT * FROM ' . $this->table . ' f WHERE accepted = 0 ORDER BY f.created DESC LIMIT ' . $first . ', 5';
             $stmt = $this->conn->prepare($query);
@@ -17,7 +17,7 @@
             return $stmt;
         }
 
-        public function read_main($page){
+        public function read_main($page) {
             $first = ($page - 1)*5;
             $query = 'SELECT * FROM ' . $this->table . ' f WHERE accepted = 1 ORDER BY f.created DESC LIMIT ' . $first . ', 5';
             $stmt = $this->conn->prepare($query);
@@ -25,14 +25,14 @@
             return $stmt;
         }
 
-        public function read_top10(){
+        public function read_top10() {
             $query = 'SELECT * FROM ' . $this->table . ' f ORDER BY f.likes DESC LIMIT 10';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
         }
 
-        public function read_admin($page){
+        public function read_admin($page) {
             $first = ($page - 1)*5;
             $query = 'SELECT * FROM ' . $this->table . ' f WHERE accepted = 0 ORDER BY f.created DESC LIMIT ' . $first . ', 5';
             $stmt = $this->conn->prepare($query);
@@ -40,35 +40,35 @@
             return $stmt;
         }
 
-        public function read_fpfti($fpfti_id){
+        public function read_fpfti($fpfti_id) {
             $query = 'SELECT * FROM ' . $this->table . ' f WHERE id = ' . $fpfti_id . '';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
         }
 
-        public function read_fpfti_tags($fpfti_id){
+        public function read_fpfti_tags($fpfti_id) { 
             $query = 'SELECT tag FROM tags WHERE fpfti_id = ' . $fpfti_id . '';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
         }
 
-        public function read_profile($user_id){
+        public function read_profile($user_id) {
             $query = 'SELECT * FROM users u WHERE id = ' . $user_id . '';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
         }
 
-        public function read_user_fpfti($user_id){
+        public function read_user_fpfti($user_id) {
             $query = 'SELECT * FROM ' . $this->table . ' f WHERE user_id = ' . $user_id . '';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
         }
 
-        public function read_user_likes($user_id){
+        public function read_user_likes($user_id) {
             $query = 'SELECT f.id, f.title, f.user_id, f.link, f.accepted, f.likes, f.created
                 FROM fpfti as f
                 JOIN likes as l
@@ -80,8 +80,16 @@
             return $stmt;
         }
 
-        public function read_fpfti_comments($fpfti_id){
+        public function read_fpfti_comments($fpfti_id) {
             $query = 'SELECT c.*, u.login FROM comments c JOIN users u ON c.user_id = u.id WHERE fpfti_id = ' . $fpfti_id . ' ORDER BY c.created DESC';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function read_search($page, $tag) {
+            $first = ($page - 1)*5;
+            $query = 'SELECT * FROM fpfti f JOIN tags t ON f.id = t.fpfti_id WHERE t.tag = \'' . $tag . '\' ORDER BY f.created DESC LIMIT ' . $first . ', 5';
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
