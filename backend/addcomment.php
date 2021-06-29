544f54c25639cb3b112a6e14e4cd6075cbf06204
Szymik <szymonszymiczek32@gmail.com>
2021-06-29 04:22:45 +0200

    coming back to meme works

--- backend/addcomment.php
+++ backend/addcomment.php
@@ -1,17 +1,30 @@
-<?php	
-    include('./includes/dbconnect.inc.php');
-    session_start();
-    
-    if ($_POST["text"] && $_POST["user-id"] && isset($_POST["fpfti-id"])) { //&& $_POST['g-recaptcha-response']) {   
-        try{
-            $text = htmlspecialchars($_POST["text"]);
-            $user_id = $_POST['user-id'];
+<?php
+    if (isset($_POST['comment-add'])) {
+        try {
+            include('./includes/dbconnect.inc.php');
+            session_start();
+
+            //getting current user info
+            $stmt1 = $dbh->prepare("SELECT * FROM users WHERE id = :id");
+            $stmt1->execute([':id' => $_SESSION['id']]);
             $fpfti_id = $_POST['fpfti-id'];
 
-            $stmt = $dbh->prepare('INSERT INTO comments SET text = :text, user_id = :user_id, fpfti_id = :fpfti_id');
-            $stmt->execute([':text' => $text, ':userid' => $userid, ':fpftiid' => $fpftiid]);
-            header("Location: https://s113.labagh.pl/index.html?page=profile&mess=commentadded");
-            exit();
+            if (empty($_POST['comment-text'])){
+                header('Location: https://s113.labagh.pl/index.html?page=fpfti&id=' . $fpfti_id . '&mess=emptycomment');
+                exit();
+            } else if ($user = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+                $stmt2 = $dbh->prepare('INSERT INTO comments (text, user_id, fpfti_id) VALUES (:text, :user_id, :fpfti_id)');
+                $stmt2->execute([
+                    ':text' => htmlspecialchars($_POST['comment-text']),
+                    ':user_id' => $_SESSION['id'],
+                    ':fpfti_id' => $fpfti_id
+                ]);
+                header('Location: https://s113.labagh.pl/index.html?page=fpfti&id=' . $fpfti_id . '&mess=commentadded');
+                exit();
+            } else {
+                header('Location: https://s113.labagh.pl/index.html?page=fpfti&id=' . $fpfti_id . '&mess=notloggedin');
+                exit();
+            }
         } catch(PDOException $e) {
             header("Location: https://s113.labagh.pl/index.html?page=profile&mess=error");
             exit();
