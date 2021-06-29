d3ef5cc066d24607638a69001c5c76ab9327e8b1
MForystek <micforystek@student.agh.edu.pl>
2021-06-29 14:33:08 +0200

    unnecessary label deleted

--- frontend/jsScripts/profile-switch.js
+++ frontend/jsScripts/profile-switch.js
@@ -4,55 +4,49 @@ function getContent(){
     const page_type = urlParams.get('page');
     return page_type;
 }
-function changeContent(content){
-    if(content == "favourite"){
+function changeContent(content) {
+    if(content == "favourite") {
         changeToFav;
-    }
-    else if(content == "added"){
+    }else if(content == "added") {
         changeToAdd;
     }
-    console.log("null0");
 }
 
-function changeToFav(){
+function changeToFav() {
     var id = getJSessionId();
     var link = 'https://s113.labagh.pl/backend/api/fpfti/read_user_fpfti.php?user_id=' + id + '';
-    $(".fpfti-template").add("");
-    //tu nie wiem czy zadziala
 
     $.ajax({
     url: link,
     type: "GET",
     dataType : "text",
     })
-    .done(function( json ) {
+    .done(function(json) {
         var tablica_json = JSON.parse(json);
         jQuery.each(tablica_json.data, function() {
             template(this.link, this.user_id, this.title, this.id, this.likes, ".fpfti-template");
         });
     })
-    .fail(function( xhr, status, errorThrown ) {
+    .fail(function(xhr, status, errorThrown) {
         alert("Data not found");
     });
 }
-function changeToAdd(){
+function changeToAdd() {
     var id = getJSessionId();
     var link = 'https://s113.labagh.pl/backend/api/fpfti/read_user_fpfti.php?user_id=' + id + '';
-    $(".fpfti-template").add("");
-    //tu nie wiem czy zadziala
     
     $.ajax({
     url: link,
     type: "GET",
     dataType : "text",
     })
-    .done(function( json ) {
+    .done(function(json) {
         var tablica_json = JSON.parse(json);
         jQuery.each(tablica_json.data, function() {
             template(this.link, this.user_id, this.title, this.id, this.likes, ".fpfti-template");
         });
     })
-    .fail(function( xhr, status, errorThrown ) {
+    .fail(function(xhr, status, errorThrown) {
         alert("Data not found");
     });
 }
